c1a711570a8c99aa74a4c985301b21822846f5fd
VasiliyDenisov <mr.v.m.denisov@mail.ru>
2023-11-20 22:20:56 +0300

    Initial

---
+++ templates/articles/show.php
@@ -0,0 +1,11 @@
+<?php require(dirname(__DIR__).'/header.html')?>
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title"><?=$article->getTitle();?></h5>
+    <p class="card-text"><?=$article->getText();?></p>
+    <p class="card-text"><?=$user->getName()?></p>
+    <a href="http://localhost/Project/www/article/edit/<?=$article->getId();?>" class="card-link">Update Article</a>
+    <a href="http://localhost/Project/www/article/delete/<?=$article->getId();?>" class="card-link">Delete Article</a>
+  </div>
+</div>
+<?php require(dirname(__DIR__).'/footer.html')?>
---
+++ templates/main/main.php
@@ -0,0 +1,23 @@
+<?php require(dirname(__DIR__).'/header.html')?>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Title</th>
+      <th scope="col">Text</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($articles as $article){
+        echo '<tr>
+            <th scope="row">1</th>
+            <td><a href="http://localhost/Project/www/article/'.$article->getId().'">'.$article->getTitle().'</a></td>
+            <td>'.$article->getText().'</td>
+            </tr>';
+    }
+    ?>
+    </tbody>
+</table>
+<?php require(dirname(__DIR__).'/footer.html')?>
+
+    
